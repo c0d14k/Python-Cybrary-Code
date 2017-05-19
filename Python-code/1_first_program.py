b28b0bc3407a3bfa19f2d49e00aa4f924ff29591
c0d14k <aslam.admani@hotmail.co.uk>
2017-05-19 08:57:55 +0100

    Create 1_first_program.py

---
+++ Python-code/1_first_program.py
@@ -0,0 +1,13 @@
+string_variable = 'hello world' #string = 'text', or "text", or '''doc string''', or """doc string""" 
+print string_variable
+
+int_variable = 1234 #int = number values
+print int_variable 
+
+#you can add/subtract/divide/multiply integers with other integers.
+#e.g.
+print '1234 + 19 =', #comma after a string means DO NOT SEPERATE WITH A NEW LINE, rather, seperate with a whitespace 
+print int_variable + 19
+#output will be: 1234 + 19 = 1253
+ 
+
